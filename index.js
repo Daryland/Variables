9a4ddae900389cf42331bc763bfb608ca25dad57
foesublime <rylandcs@gmail.com>
2022-12-21 14:00:12 -0700

    Variables for JavaScript

---
+++ index.js
@@ -0,0 +1,29 @@
+// Variables in JavaScript
+
+// (var) - prior ES2015 / ES6 
+
+var userName = 'Value';
+
+var totalWins = 30;
+
+
+//  (let, const) -ES2015 < ES6
+var totalLosses = 10;
+
+let name = 'Daniel';
+
+const age = 25;
+
+
+// Variable Rules 
+
+// Should have a meaningful name 
+
+// Cannot contain hyphen or space
+
+// Cannot start with a number
+
+// Cannot use a reserve word
+var let if true false 
+
+// Variables are case sensitive
